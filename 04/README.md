f9c5700cd8093d2525a37611b06899220894b608
Ricardo Alves <ricardospalves@gmail.com>
2022-01-27 12:33:37 -0300

    Adicionar a aula 4

---
+++ 04/README.md
@@ -0,0 +1,3 @@
+# 04 - Type Aliases e Union
+
+[Videoaula](https://www.youtube.com/watch?v=DqpBryBjs6Q&list=PLlAbYrWSYTiPanrzauGa7vMuve7_vnXG_&index=7)
---
+++ 04/alias.ts
@@ -0,0 +1,24 @@
+// Type alias
+type Uid = number | string
+
+function logDetails(uid: Uid, title: string) {
+  console.log(`A product with ${uid} has a title as ${title}.`)
+}
+
+function logInfo(uid: Uid, usename: string) {
+  console.log(`An user with ${uid} has a name as ${usename}.`)
+}
+
+logDetails(123, 'lorem')
+logDetails('123', 'lorem')
+
+logInfo(789, 'ipsum')
+logInfo('789', 'ipsum')
+
+type Platform = 'Windows' | 'Linux' | 'Mac Os'
+
+function renderPlataform(platform: Platform) {
+  return platform
+}
+
+renderPlataform('Linux')
